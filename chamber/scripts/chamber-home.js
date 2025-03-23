46d525cd95c59772959481b131e6a9ef8f4354a4
Jorge Sosa <joregesosa@gmail.com>
2025-03-22 20:44:14 -0400

    Remove obsolete toggle button from business directory and clean up imports

--- chamber/scripts/chamber-home.js
+++ chamber/scripts/chamber-home.js
@@ -1,8 +1,8 @@
 import { getBusiness, getWeather } from './utils.js';
+
 const bissnessDirectory = document.querySelector('#business_directory');
-const toggleGrid = document.querySelector('#business_directory > button'); 
 const currentWeather = document.querySelector('#current_weather');
-const forecastWeather = document.querySelector('#weather_forecast');
-toggleGrid.remove();
+const forecastWeather = document.querySelector('#weather_forecast'); 
+
 getWeather(currentWeather, forecastWeather);
 getBusiness(bissnessDirectory, 3);
