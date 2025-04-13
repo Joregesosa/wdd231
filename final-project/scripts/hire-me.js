5af58b447f96ec448727b54210e26c80f94e4896
Jorge Sosa <joregesosa@gmail.com>
2025-04-12 21:17:18 -0400

    Enhance hire-me page with improved metadata, form handling, and dialog functionality; update styles for better user experience

---
+++ final-project/scripts/hire-me.js
@@ -0,0 +1,46 @@
+const hireMeForm = document.querySelector('#hire-me-form');
+const resumeDialog = document.querySelector('#resume-dialog');
+
+hireMeForm.addEventListener('change', (event) => {
+    const formData = new FormData(hireMeForm);
+    const data = Object.fromEntries(formData.entries());
+    localStorage.setItem('hireMeData', JSON.stringify(data));
+});
+
+hireMeForm.addEventListener('submit', (event) => {  
+    const formData = new FormData(hireMeForm);
+    const data = Object.fromEntries(formData.entries());
+    console.log('Form submitted:', data); 
+    localStorage.removeItem('hireMeData');
+    
+});
+
+resumeDialog.addEventListener('click', (event) => {
+    const { target } = event;
+    const { tagName, value } = target;
+
+    if (tagName === 'BUTTON' && value === 'no-resume-form') {
+        localStorage.removeItem('hireMeData');
+        resumeDialog.close();
+        return;
+    }
+
+    if (tagName === 'BUTTON' && value === 'resume-form') {
+        const savedData = localStorage.getItem('hireMeData');
+        if (savedData) {
+            const data = JSON.parse(savedData);
+            for (const [key, value] of Object.entries(data)) {
+                hireMeForm[key].value = value;
+            }
+        }
+        resumeDialog.close();
+    }
+
+});
+
+onload = () => {
+    const savedData = localStorage.getItem('hireMeData');
+    if (savedData) {
+        resumeDialog.showModal();
+    }
+}
